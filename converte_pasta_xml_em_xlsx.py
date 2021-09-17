ae63631397d774ba8be42f893a1388f7d5c896c5
Diogo Caetano Garcia <srcaetano@gmail.com>
2021-09-16 22:33:36 -0300

    Atualizando funcionalidades

--- converte_pasta_xml_em_xlsx.py
+++ converte_pasta_xml_em_xlsx.py
@@ -4,25 +4,31 @@ from openpyxl import Workbook
 import time
 
 def find_fields(xml_file_name, elms_attrs):
-	xmldoc = minidom.parse(xml_file_name)
+	xmldoc = minidom.parse(xml_file_name) # Read XML file
 	cur_list = []
 	for elms in elms_attrs:
-		itemlist = xmldoc.getElementsByTagName(elms[1])
-		if (len(itemlist)==0) or (not itemlist[0].getAttribute(elms[2])):
-			if elms[3]:
-				cur_list.append("0")
-			else:
+		itemlist = xmldoc.getElementsByTagName(elms[1]) # Look for tag
+		if elms[3]: # elms[3]==True indicates we want a mere count of this tag
+			cur_list.append(len(itemlist))
+		else:       # elms[3]==False indicates we want a string with all found tags and attributes
+			if (len(itemlist)==0): # Tag not found
 				cur_list.append('')
-		else:
-			if elms[3]:
-				cur_list.append(str(len(itemlist)))
-			else:
-				if len(itemlist)==1:
-					cur_list.append(itemlist[0].attributes[elms[2]].value)
+			else: # Tag found
+				attrs = [i.getAttribute(cur_attr) for i in itemlist for cur_attr in elms[2]]
+				s = '/'
+				if elms[4]:
+					l = len(elms[2])
+					try:
+						cur_index = attrs[0:-1:l].index('')
+					except ValueError:
+						cur_index = -1
+					if cur_index!=1:
+						cur_index *= l
+						cur_list.append(s.join(attrs[cur_index+1:cur_index+l]))
+					else:
+						cur_list.append('')
 				else:
-					vals = [il.attributes[elms[2]].value for il in itemlist]
-					s = ', '
-					cur_list.append(s.join(list(set(vals))))
+					cur_list.append(s.join(attrs))
 	return cur_list
 
 def xmls_2_xlsx(xml_file_folder, elms_attrs, output_file_name):
@@ -45,39 +51,47 @@ def xmls_2_xlsx(xml_file_folder, elms_attrs, output_file_name):
 	wb.save(newfilename)
 	return
 
-folder = "data/2021_04/" #Dados/"
-# folder = "Testes/"
+folder = "data/2021_04/"
+folder = "data/"
 
-xml_fields = [
-	['Nome','DADOS-GERAIS','NOME-COMPLETO', False],
-	['País de nascimento','DADOS-GERAIS','PAIS-DE-NASCIMENTO', False],
-	['Data de atualização', 'CURRICULO-VITAE', 'DATA-ATUALIZACAO', False],
-	['Id Lattes', 'CURRICULO-VITAE', 'NUMERO-IDENTIFICADOR', False],
-	['País de atuação', 'ENDERECO-PROFISSIONAL', 'PAIS', False],
-	['Cidade', 'ENDERECO-PROFISSIONAL', 'CIDADE', False],
-	['Estado', 'ENDERECO-PROFISSIONAL', 'UF', False],
-	['Instituição', 'ENDERECO-PROFISSIONAL', 'NOME-INSTITUICAO-EMPRESA', False],
-	['Órgão', 'ENDERECO-PROFISSIONAL', 'NOME-ORGAO', False],
-	['Unidade', 'ENDERECO-PROFISSIONAL', 'NOME-UNIDADE', False],
-	['CEP', 'ENDERECO-PROFISSIONAL', 'CEP', False],
-	['Doutorado', 'DOUTORADO', 'ANO-DE-CONCLUSAO', False],
-	['Doutorado', 'DOUTORADO', 'ANO-DE-OBTENCAO-DO-TITULO', False],
-	['Mestrado', 'MESTRADO', 'ANO-DE-CONCLUSAO', False],
-	['Mestrado', 'MESTRADO', 'ANO-DE-OBTENCAO-DO-TITULO', False],
-	['Especialização', 'ESPECIALIZACAO', 'ANO-DE-CONCLUSAO', False],
-	['Graduação', 'GRADUACAO', 'ANO-DE-CONCLUSAO', False],
-	['Grande área de atuação', 'AREA-DE-ATUACAO', 'NOME-GRANDE-AREA-DO-CONHECIMENTO', False],
-	['Área de atuação', 'AREA-DE-ATUACAO', 'NOME-DA-AREA-DO-CONHECIMENTO', False],
-	['Sub-área de atuação', 'AREA-DE-ATUACAO', 'NOME-DA-SUB-AREA-DO-CONHECIMENTO', False],
-	['Especialidade', 'AREA-DE-ATUACAO', 'NOME-DA-ESPECIALIDADE', False],
-	['Trabalhos em eventos', 'TRABALHO-EM-EVENTOS', '', True],
-	['Artigos publicados', 'ARTIGO-PUBLICADO', '', True],
-	['Livros e capítulos', 'CAPITULO-DE-LIVRO-PUBLICADO', '', True],
-	['Patentes', 'PATENTE', '', True],
-	['Processos ou técnicas', 'PROCESSOS-OU-TECNICAS', '', True],
-	['Trabalho técnico', 'TRABALHO-TECNICO', '', True],
-	['Orientações (doutorado)', 'ORIENTACOES-CONCLUIDAS-PARA-DOUTORADO', '', True],
-	['Orientações (mestrado)', 'ORIENTACOES-CONCLUIDAS-PARA-MESTRADO', '', True],
-	['Orientações (outras)', 'OUTRAS-ORIENTACOES-CONCLUIDAS', '', True],
+xml_fields = [['Nome','DADOS-GERAIS',['NOME-COMPLETO'], False, False],
+	['País de nascimento','DADOS-GERAIS',['PAIS-DE-NASCIMENTO'], False, False],
+	['Data de atualização', 'CURRICULO-VITAE', ['DATA-ATUALIZACAO'], False, False],
+	['Id Lattes', 'CURRICULO-VITAE', ['NUMERO-IDENTIFICADOR'] , False, False],
+	['Instituição de atuação', 'ENDERECO-PROFISSIONAL', ['NOME-INSTITUICAO-EMPRESA','NOME-ORGAO','NOME-UNIDADE'], False, False],
+	['Vínculo', 'VINCULOS', ['ANO-FIM','OUTRO-ENQUADRAMENTO-FUNCIONAL-INFORMADO','OUTRO-VINCULO-INFORMADO','TIPO-DE-VINCULO'], False, True],
+	['País de atuação', 'ENDERECO-PROFISSIONAL', ['PAIS'], False, False],
+	['Cidade', 'ENDERECO-PROFISSIONAL', ['CIDADE'], False, False],
+	['Estado', 'ENDERECO-PROFISSIONAL', ['UF'], False, False],
+	['CEP', 'ENDERECO-PROFISSIONAL', ['CEP'], False, False],
+	['Doutorado (instituição/código/conclusão/obtenção)', 'DOUTORADO', ['NOME-INSTITUICAO','CODIGO-INSTITUICAO','ANO-DE-CONCLUSAO','ANO-DE-OBTENCAO-DO-TITULO'], False, False],
+	['Mestrado (instituição/código/conclusão/obtenção)', 'MESTRADO', ['NOME-INSTITUICAO','CODIGO-INSTITUICAO','ANO-DE-CONCLUSAO','ANO-DE-OBTENCAO-DO-TITULO'], False, False],
+	['Especialização (instituição/código/conclusão/obtenção)', 'ESPECIALIZACAO', ['NOME-INSTITUICAO','CODIGO-INSTITUICAO','ANO-DE-CONCLUSAO','ANO-DE-OBTENCAO-DO-TITULO'], False, False],
+	['Graduação (instituição/código/conclusão/obtenção)', 'GRADUACAO', ['NOME-INSTITUICAO','CODIGO-INSTITUICAO','ANO-DE-CONCLUSAO','ANO-DE-OBTENCAO-DO-TITULO'], False, False],
+	['Grande área de atuação', 'AREA-DE-ATUACAO', ['NOME-GRANDE-AREA-DO-CONHECIMENTO'], False, False],
+	['Área de atuação', 'AREA-DE-ATUACAO', ['NOME-DA-AREA-DO-CONHECIMENTO'], False, False],
+	['Sub-área de atuação', 'AREA-DE-ATUACAO', ['NOME-DA-SUB-AREA-DO-CONHECIMENTO'], False, False],
+	['Especialidade', 'AREA-DE-ATUACAO', ['NOME-DA-ESPECIALIDADE'], False, False],
+	['Trabalhos em eventos', 'TRABALHO-EM-EVENTOS', [''], True, False],
+	['Artigos publicados', 'ARTIGO-PUBLICADO', [''], True, False],
+	['Livros e capítulos', 'CAPITULO-DE-LIVRO-PUBLICADO', [''], True, False],
+	['Participação em projetos', 'PARTICIPACAO-EM-PROJETO', [''], True, False],
+	['Patentes', 'PATENTE', [''], True, False],
+	['Processos ou técnicas', 'PROCESSOS-OU-TECNICAS', [''], True, False],
+	['Trabalho técnico', 'TRABALHO-TECNICO', [''], True, False],
+	['Orientações (doutorado)', 'ORIENTACOES-CONCLUIDAS-PARA-DOUTORADO', [''], True, False],
+	['Orientações (mestrado)', 'ORIENTACOES-CONCLUIDAS-PARA-MESTRADO', [''], True, False],
+	['Orientações (outras)', 'OUTRAS-ORIENTACOES-CONCLUIDAS', [''], True, False],
 	]
+
+# País de atuação vs país de graduação - atuação ok, graduação não
+# Incluir códigos das instituições (graduação, mestrado e doutorado) - ok
+# Conferir origem desses códigos (Google, email ou falar com fuinha)
+#    http://di.cnpq.br/di/index.jsp
+#    http://lattes.cnpq.br/web/dgp/por-instituicao4
+# Instituição atual onde trabalha é o mais importante. Vínculo atual é importante também - ok
+# Incluir contagem de projetos - ok
+# Separar brasileiros formados na Itália (graduação etc.)
+# EXTRA: Buscar Web of science e Scopus
+# https://openpyxl.readthedocs.io/en/stable/usage.html
 xmls_2_xlsx(folder, xml_fields, "xml_to_excel.xlsx")
